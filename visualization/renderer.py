d7b9c1cc90ba1e1f639939d77722f82c7fe07bd8
Cvet0mir <146551250+Cvet0mir@users.noreply.github.com>
2026-03-26 22:09:26 +0200

    Made the character move horizontally only

--- visualization/renderer.py
+++ visualization/renderer.py
@@ -26,10 +26,6 @@ def render_game():
         draw_stickman(screen, player_pos)
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            player_pos.y -= 300 * dt
-        if keys[pygame.K_s]:
-            player_pos.y += 300 * dt
         if keys[pygame.K_a]:
             player_pos.x -= 300 * dt
         if keys[pygame.K_d]:
